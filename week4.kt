8f80217bde0e04b20b291a660a4fec7ca81fa12f
noahgreenmusic <85235360+noahgreenmusic@users.noreply.github.com>
2021-06-29 11:42:12 -0700

    Add files via upload

---
+++ week4.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val a: Int = 2
+    val b: Int = 4
+    val c: Int = 6
+    val d: Int = 3
+    if (c-b == b-a ) {println{"True"}
+        else if (c-b == c-d) {println("False")}
+}
